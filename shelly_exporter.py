3f974fae0cf9fb6a612b3b8208983c2d99e23a5a
Thomas GUIRRIEC <thomas@guirriec.fr>
2023-12-08 04:47:37 +0000

    Feat: Update Alpine 3.19

--- shelly_exporter.py
+++ shelly_exporter.py
@@ -18,7 +18,7 @@ import pytz
 import requests
 from prometheus_client import PLATFORM_COLLECTOR, PROCESS_COLLECTOR
 from prometheus_client.core import REGISTRY, CollectorRegistry, Metric
-from prometheus_client.exposition import _bake_output, parse_qs
+from prometheus_client.exposition import _bake_output, _SilentHandler, parse_qs
 
 SHELLY_EXPORTER_NAME = os.environ.get("SHELLY_EXPORTER_NAME", "shelly-exporter")
 SHELLY_EXPORTER_LOGLEVEL = os.environ.get("SHELLY_EXPORTER_LOGLEVEL", "INFO").upper()
@@ -74,7 +74,7 @@ def start_wsgi_server(
 ) -> None:
     """Starts a WSGI server for prometheus metrics as a daemon thread."""
     app = make_wsgi_app(registry)
-    httpd = make_server(addr, port, app)
+    httpd = make_server(addr, port, app, handler_class=_SilentHandler)
     thread = threading.Thread(target=httpd.serve_forever)
     thread.daemon = True
     thread.start()
